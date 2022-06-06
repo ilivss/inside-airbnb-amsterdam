3d4d5005655d5135510d6963b463635a7d91aefc
iliass <git@iek.dev>
2022-06-06 12:00:12 +0200

    Fix ValidIssuer value

--- api/Program.cs
+++ api/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         c.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
         {
             ValidAudience = builder.Configuration["Auth0:Audience"],
-            ValidIssuer = $"https://{builder.Configuration["Auth0:Domain"]}"
+            ValidIssuer = builder.Configuration["Auth0:Domain"]
         };
     });
 
