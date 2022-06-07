13829c35d5d6779dc84c41930bbdd2d73fc53f0f
iliass <git@iek.dev>
2022-06-07 16:21:50 +0200

    Seperate http clients.

--- client/Program.cs
+++ client/Program.cs
@@ -8,28 +8,23 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("APIBaseAddress")) });
-
-builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("APIBaseAddress")))
+// Inject HttpClient into components
+builder.Services.AddHttpClient<IListingService, ListingService>(client => { client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("APIBaseAddress")); });
+builder.Services.AddHttpClient<INeighbourhoodService, NeighbourhoodService>(client => { client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("APIBaseAddress")); });
+builder.Services.AddHttpClient<IStatisticsService, StatisticsService>(client => client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("APIBaseAddress")))
                 .AddHttpMessageHandler(sp => sp.GetRequiredService<AuthorizationMessageHandler>()
-                .ConfigureHandler(
-                    authorizedUrls: new[] { builder.Configuration.GetValue<string>("APIBaseAddress") }
-                ));
+                                               .ConfigureHandler(authorizedUrls: new[] { builder.Configuration.GetValue<string>("APIBaseAddress") })
+                                      );
 
-builder.Services.AddSingleton(sp => sp.GetRequiredService<IHttpClientFactory>()
-                .CreateClient("ServerAPI"));
+// Services
+builder.Services.AddScoped<IMapboxService, MapboxService>();
 
+// Auth0
 builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Auth0", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
     options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration.GetValue<string>("Auth0:Audience"));
 });
-
-// Services
-builder.Services.AddSingleton<IListingService, ListingService>();
-builder.Services.AddSingleton<INeighbourhoodService, NeighbourhoodService>();
-builder.Services.AddSingleton<IStatisticsService, StatisticsService>();
-builder.Services.AddSingleton<IMapboxService, MapboxService>();
 
 await builder.Build().RunAsync();
